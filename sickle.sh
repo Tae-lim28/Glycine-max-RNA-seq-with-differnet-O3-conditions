e8d12b78c1afe861aabf24280860c3a16abb4e9c
Tae-lim28 <tktntodtjdnfeo@gmail.com>
2022-12-20 01:19:08 +0900

    first commit

---
+++ sickle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## This code is for trimmimg of six fastq files. ##
+PROGRAM="sickle"
+MODE="se"
+QUAL_TYPE="sanger"
+LEN_THR="45"
+QUAL_THR="35"
+SAMPLE=("SRR391535" "SRR391536" " SRR391537" " SRR391538" " SRR391539" " SRR391541")
+######################################
+
+for i in "${SAMPLE[@]}"
+do
+    CMD="${PROGRAM} ${MODE} -f 01_in_fq/${i}.fastq -t ${QUAL_TYPE} -o 02_trimmed_fq/trimmed_${i}.fastq -q ${QUAL_THR} -l ${LEN_THR} --gzip-output"
+    echo ${CMD}
+    eval ${CMD}
+done
