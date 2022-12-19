2fb4cb03bcc932c2325ea863dbbe0b1d0daf8305
Tae-lim28 <tktntodtjdnfeo@gmail.com>
2022-12-20 03:06:52 +0900

    first commit

--- sickle.sh
+++ sickle.sh
@@ -6,12 +6,12 @@ MODE="se"
 QUAL_TYPE="sanger"
 LEN_THR="45"
 QUAL_THR="35"
-SAMPLE=("SRR391535" "SRR391536" " SRR391537" " SRR391538" " SRR391539" " SRR391541")
+SAMPLE=("SRR391535" "SRR391536" "SRR391537" "SRR391538" "SRR391539" "SRR391541")
 ######################################
 
 for i in "${SAMPLE[@]}"
 do
-    CMD="${PROGRAM} ${MODE} -f 01_in_fq/${i}.fastq -t ${QUAL_TYPE} -o 02_trimmed_fq/trimmed_${i}.fastq -q ${QUAL_THR} -l ${LEN_THR} --gzip-output"
+    CMD="${PROGRAM} ${MODE} -f 01_in_fq/${i}.fastq -t ${QUAL_TYPE} -o 02_trimmed_fq/trimmed_${i}.fastq.gz -q ${QUAL_THR} -l ${LEN_THR} --gzip-output"
     echo ${CMD}
     eval ${CMD}
 done
